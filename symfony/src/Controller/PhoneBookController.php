2ac4ee61ff5c173fbc9a7bff3a3ac02b75c20145
Petrow Danil <d.petrov@qsoft.ru>
2021-09-26 19:50:09 +0300

    jwt token auth added

--- symfony/src/Controller/PhoneBookController.php
+++ symfony/src/Controller/PhoneBookController.php
@@ -4,63 +4,75 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-use FOS\RestBundle\Controller\Annotations as Rest;
+// use FOS\RestBundle\Controller\Annotations as Rest;
+// use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
 class PhoneBookController extends AbstractController
 {
 
     /**
-     * @Rest\Get("/api/phone-book/{$id}")
+     * @Route("/api/phone-book", name="api_get_phone_book")
      */
-    public function getPhoneBookAction(): Response
+    public function getPhoneBookAction()
     {
-        return $this->json([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/PhoneBookController.php',
-        ]);
+        var_dump(1);
+        die();
     }
 
-    /**
-     * @Rest\Get("/api/phone-book/")
-     */
-    public function getPhoneBooksAction(): Response
-    {
-        return $this->json([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/PhoneBookController.php',
-        ]);
-    }
 
-    /**
-     * @Rest\Post("/api/phone-book")
-     */
-    public function addPhoneBookAction(): Response
-    {
-        return $this->json([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/PhoneBookController.php',
-        ]);
-    }
+    // /**
+    //  * @Rest\Get("/api/phone-book/{$id}")
+    //  */
+    // public function getPhoneBookAction(): Response
+    // {
+    //     return $this->json([
+    //         'message' => 'Welcome to your new controller!',
+    //         'path' => 'src/Controller/PhoneBookController.php',
+    //     ]);
+    // }
 
-    /**
-     * @Rest\Patch("/api/phone-book")
-     */
-    public function updatePhoneBookAction(): Response
-    {
-        return $this->json([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/PhoneBookController.php',
-        ]);
-    }
+    // /**
+    //  * @Rest\Get("/api/phone-book/")
+    //  */
+    // public function getPhoneBooksAction(): Response
+    // {
+    //     return $this->json([
+    //         'message' => 'Welcome to your new controller!',
+    //         'path' => 'src/Controller/PhoneBookController.php',
+    //     ]);
+    // }
 
-    /**
-     * @Rest\Delete("/api/phone-book")
-     */
-    public function deletePhoneBookAction(): Response
-    {
-        return $this->json([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/PhoneBookController.php',
-        ]);
-    }
+    // /**
+    //  * @Rest\Post("/api/phone-book")
+    //  */
+    // public function addPhoneBookAction(): Response
+    // {
+    //     return $this->json([
+    //         'message' => 'Welcome to your new controller!',
+    //         'path' => 'src/Controller/PhoneBookController.php',
+    //     ]);
+    // }
+
+    // /**
+    //  * @Rest\Patch("/api/phone-book")
+    //  */
+    // public function updatePhoneBookAction(): Response
+    // {
+    //     return $this->json([
+    //         'message' => 'Welcome to your new controller!',
+    //         'path' => 'src/Controller/PhoneBookController.php',
+    //     ]);
+    // }
+
+    // /**
+    //  * @Rest\Delete("/api/phone-book")
+    //  */
+    // public function deletePhoneBookAction(): Response
+    // {
+    //     return $this->json([
+    //         'message' => 'Welcome to your new controller!',
+    //         'path' => 'src/Controller/PhoneBookController.php',
+    //     ]);
+    // }
 }
