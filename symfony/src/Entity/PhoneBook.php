ea0a5f1ae750db075612672237f6857510d70fe9
Petrow Danil <d.petrov@qsoft.ru>
2021-09-27 11:33:37 +0300

    entity serializer added

--- symfony/src/Entity/PhoneBook.php
+++ symfony/src/Entity/PhoneBook.php
@@ -106,4 +106,23 @@ class PhoneBook
 
         return $this;
     }
+
+    /**
+     * Specify data which should be serialized to JSON
+     * @link https://php.net/manual/en/jsonserializable.jsonserialize.php
+     * @return mixed data which can be serialized by <b>json_encode</b>,
+     * which is a value of any type other than a resource.
+     * @since 5.4.0
+     */
+    public function jsonSerialize()
+    {
+        return [
+            'id' => $this->getId(),
+            'first_name' => $this->getFirstName(),
+            'last_name' => $this->getLastName(),
+            'phone_number' => $this->getPhoneNumber(),
+            'country_code' => $this->getCountryCode(),
+            'time_zone' => $this->getTimeZone()
+        ];
+    }
 }
